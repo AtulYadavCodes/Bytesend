bdb259866cbcc2b730727229337e4b9c35565ced
Atul Yadav <atul99yadav99@gmail.com>
2026-05-01 14:24:21 +0530

    spacing and format

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import { NavLink } from 'react-router'
 
 function Navbar() {
-  const color="";
+  const color = "";
   return (
     <div className="w-full flex justify-center py-4 font-mono">
-  <div className="flex bg-[var(--term-panel)] border border-[var(--term-border)] rounded-md p-1 w-[90vw] lg:w-[40vw]">
+      <div className="flex bg-[var(--term-panel)] border border-[var(--term-border)] rounded-md p-1 w-[90vw] lg:w-[40vw]">
 
-    <NavLink
-      to="/"
-      className={({ isActive }) =>`flex-1 text-center py-2 text-sm transition ${ isActive   ? "bg-[var(--term-green)] text-black"   : "text-[var(--term-text)] hover:text-white" }`
-      }
-    >
-      [ files ]
-    </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) => `flex-1 text-center py-2 text-sm transition ${isActive ? "bg-[var(--term-green)] text-black" : "text-[var(--term-text)] hover:text-white"}`
+          }
+        >
+          [ files ]
+        </NavLink>
 
-    <NavLink
-      to="/text"
-      className={({ isActive }) => `flex-1 text-center py-2 text-sm transition ${   isActive     ? "bg-[var(--term-green)] text-black"     : "text-[var(--term-text)] hover:text-white"  }`
-      }
-    >
-      [ text ]
-    </NavLink>
+        <NavLink
+          to="/text"
+          className={({ isActive }) => `flex-1 text-center py-2 text-sm transition ${isActive ? "bg-[var(--term-green)] text-black" : "text-[var(--term-text)] hover:text-white"}`
+          }
+        >
+          [ text ]
+        </NavLink>
 
-  </div>
-</div>
+      </div>
+    </div>
   )
 }
 
